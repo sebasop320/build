b9947b96d10c497b2f50ce51e386295c288b29b8
Sebastian Ocampo <73962362+sebasop320@users.noreply.github.com>
2023-01-15 19:17:28 -0500

    Add files via upload

---
+++ client/_app/immutable/components/pages/_page.svelte-ab059d0c.js
@@ -0,0 +1,15 @@
+import{S as W,i as Y,s as j,k as l,a as M,q as B,L as D,l as o,h as t,c as O,m as v,r as z,n as e,E as G,F as a,b as P,B as I}from"../../chunks/index-6c33eb69.js";import{u as J}from"../../chunks/user-7d2574ba.js";const K=""+new URL("../../assets/svelte-welcome-c18bcf5a.webp",import.meta.url).href,Q=""+new URL("../../assets/svelte-welcome-6c300099.png",import.meta.url).href;function V(N){let m,p,d,f,w,r,h,L,A,b,k,x,c,_,i,E,C,u,R,H,S;return{c(){m=l("meta"),p=l("meta"),d=l("meta"),f=l("meta"),w=M(),r=l("h1"),h=l("img"),A=B(`
+	Hi i'm Sebasop320 an enthusiast developer 
+		`),b=l("style"),k=B(`h1{
+			color: #f25416;
+			font-family: 'Bold'
+			font-weight: 'bold';
+			font-size: 4em;
+		}`),x=M(),c=l("section"),_=l("span"),i=l("picture"),E=l("source"),C=M(),u=l("img"),H=M(),S=l("h2"),this.h()},l(s){const n=D("svelte-1w1rvdk",document.head);m=o(n,"META",{title:!0}),p=o(n,"META",{name:!0,content:!0}),d=o(n,"META",{name:!0,content:!0}),f=o(n,"META",{name:!0,content:!0}),n.forEach(t),w=O(s),r=o(s,"H1",{class:!0});var y=v(r);h=o(y,"IMG",{src:!0,alt:!0}),A=z(y,`
+	Hi i'm Sebasop320 an enthusiast developer 
+		`),b=o(y,"STYLE",{});var U=v(b);k=z(U,`h1{
+			color: #f25416;
+			font-family: 'Bold'
+			font-weight: 'bold';
+			font-size: 4em;
+		}`),U.forEach(t),y.forEach(t),x=O(s),c=o(s,"SECTION",{class:!0});var g=v(c);_=o(g,"SPAN",{class:!0});var q=v(_);i=o(q,"PICTURE",{});var T=v(i);E=o(T,"SOURCE",{srcset:!0,type:!0}),C=O(T),u=o(T,"IMG",{src:!0,alt:!0,class:!0}),T.forEach(t),q.forEach(t),H=O(g),S=o(g,"H2",{});var F=v(S);F.forEach(t),g.forEach(t),this.h()},h(){document.title="Sebasop320 --- Portolio",e(m,"title","Sebasopdev---Sebastian Ocampo"),e(p,"name","description"),e(p,"content","Personal Portfolio"),e(d,"name","keywords"),e(d,"content","portfolio, Sebastian Ocampo, sebastian Ocampo, developer in panama, desarrollador en panama, desarollador "),e(f,"name","author"),e(f,"content","Sebastian Ocampo"),G(h.src,L=J)||e(h,"src",L),e(h,"alt","user icon"),e(r,"class","svelte-19xx0bt"),e(E,"srcset",K),e(E,"type","image/webp"),G(u.src,R=Q)||e(u,"src",R),e(u,"alt","Welcome"),e(u,"class","svelte-19xx0bt"),e(_,"class","welcome svelte-19xx0bt"),e(c,"class","svelte-19xx0bt")},m(s,n){a(document.head,m),a(document.head,p),a(document.head,d),a(document.head,f),P(s,w,n),P(s,r,n),a(r,h),a(r,A),a(r,b),a(b,k),P(s,x,n),P(s,c,n),a(c,_),a(_,i),a(i,E),a(i,C),a(i,u),a(c,H),a(c,S)},p:I,i:I,o:I,d(s){t(m),t(p),t(d),t(f),s&&t(w),s&&t(r),s&&t(x),s&&t(c)}}}class $ extends W{constructor(m){super(),Y(this,m,null,V,j,{})}}export{$ as default};
--- server/manifest.js
+++ server/manifest.js
@@ -1,10 +1,10 @@
 const manifest = {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png","robots.txt"]),
+	assets: new Set([".DS_Store","favicon.png","old-favicon.png","robots.txt"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain"},
 	_: {
-		entry: {"file":"_app/immutable/start-8e3d7c38.js","imports":["_app/immutable/start-8e3d7c38.js","_app/immutable/chunks/index-6c33eb69.js","_app/immutable/chunks/singletons-08e7a750.js","_app/immutable/chunks/parse-f0c618df.js"],"stylesheets":[],"fonts":[]},
+		entry: {"file":"_app/immutable/start-c74536fc.js","imports":["_app/immutable/start-c74536fc.js","_app/immutable/chunks/index-6c33eb69.js","_app/immutable/chunks/singletons-08e7a750.js","_app/immutable/chunks/parse-f0c618df.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./chunks/0-bcea7804.js'),
 			() => import('./chunks/1-31eb2623.js'),
