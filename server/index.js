2dfc5b756a7d4106d9af9969f4498739c2420393
Sebastian Ocampo <73962362+sebasop320@users.noreply.github.com>
2023-02-02 18:23:15 -0500

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -3075,7 +3075,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1674828313094"
+      version: "1674837753878"
     };
   }
   /**
