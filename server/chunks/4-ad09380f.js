2ce652ebb98fdd45d28f66f51afbbf7bd7cf1007
Sebastian Ocampo <73962362+sebasop320@users.noreply.github.com>
2023-02-14 15:07:15 -0500

    V 0.2.10

---
+++ server/chunks/4-ad09380f.js
@@ -0,0 +1,9 @@
+const index = 4;
+const component = async () => (await import('./_page.svelte-4da714d7.js')).default;
+const file = '_app/immutable/components/pages/colegio/_page.svelte-74500bcd.js';
+const imports = ["_app/immutable/components/pages/colegio/_page.svelte-74500bcd.js","_app/immutable/chunks/index-6c33eb69.js"];
+const stylesheets = [];
+const fonts = [];
+
+export { component, file, fonts, imports, index, stylesheets };
+//# sourceMappingURL=4-ad09380f.js.map
